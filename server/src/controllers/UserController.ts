b4ba5e42efb78f8a986cc4e4f7a96b2880c6bc6d
Kolotylo Kyrylo <kolot102@gmail.com>
2023-07-27 17:19:01 +0300

    add isCurrentUser property to getUserById response

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -1,17 +1,25 @@
 import UserDTO from '@/dto/UserDTO';
+import { tokenService } from '@/services/TokenService';
 import UserService from '@/services/UserService';
 import { NextFunction, Request, Response } from 'express';
 
 class userController {
     async getUserById(req: Request, res: Response, next: NextFunction) {
         try {
+            const { refreshToken } = req.cookies;
             const { id } = req.params;
 
             const user = await UserService.getUserById(id);
+            const responser = await tokenService.validateRefreshToken(
+                refreshToken
+            );
 
             const userDto = new UserDTO(user);
 
-            res.status(200).json(userDto);
+            res.status(200).json({
+                userDto,
+                isCurrentUser: responser.id === user.id,
+            });
         } catch (error) {
             next(error);
         }
